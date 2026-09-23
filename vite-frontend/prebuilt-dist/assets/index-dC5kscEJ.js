2022d96b4d6a3fc138d27c7a58fb6937715d65a6
w243420707 <20176890+w243420707@users.noreply.github.com>
2026-09-23 23:20:51 +0800

    fix: refresh bundled frontend assets

--- vite-frontend/prebuilt-dist/assets/index-dC5kscEJ.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./features-animation-ClqXO_W8.js";import"./index-BbIW8G29.js";import"./accelerated-values-URi8PXxO.js";var m=o;export{m as default};
---
+++ vite-frontend/prebuilt-dist/assets/index-f2MqYz3F.js
@@ -0,0 +1,1 @@
+import{d as o}from"./features-animation-DV0y5jjz.js";import"./index-0VxiO_9o.js";import"./accelerated-values-CoatAZaw.js";var m=o;export{m as default};
